c01c5823f4215d5243e59a138fa14127b8df2406
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-06-27 13:26:46 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _printf - prints a string
+ * @format: string to print
+ *
+ * Return: string
+ */
+int _printf(const char *format, ...)
+{
+	char *buffer = malloc(sizeof(char) * 1024);
+	char *specifier_buff = malloc(sizeof(char) * 1024);
+	int size, i = 0;
+	va_list ap;
+
+	va_start(ap, format);
+
+	size = my_print(format, ap, buffer, specifier_buff);
+	while (buffer[i] != '\0')
+	{
+		_putchar(buffer[i]);
+		i++;
+	}
+	va_end(ap);
+	free(buffer);
+	free(specifier_buff);
+	return (size);
+}
