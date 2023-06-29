b512d20b9e57e8bc868795c1a1a908963d58c570
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-06-29 22:38:25 +0100

    Create print_hex.c

---
+++ print_hex.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_hex - adds a hex number to the buffer
+ * @args: va_list containing the number to print
+ * Return: string of hex number
+ */
+char *print_hex(va_list args)
+{
+        char *str;
+        unsigned int num = va_arg(args, unsigned int);
+
+        str = malloc(sizeof(char) * 100);
+        if (!str)
+                return (NULL);
+        _itoa(num, str, 16);
+        return (str);
+}
