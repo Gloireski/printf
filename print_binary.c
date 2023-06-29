a40e30f797dfedda37ef3069798092b0cde52441
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-06-29 22:36:42 +0100

    Create print_binary.c

---
+++ print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_binary - adds a binary number to a buffer
+ * @args: argument list
+ * 
+ * Return: string of the number
+ */
+char *print_binary(va_list args)
+{
+char *str;
+        unsigned int num = va_arg(args, unsigned int);
+
+        str = malloc(sizeof(char) * 100);
+        if (!str)
+                return (NULL);
+        _itoa(num, str, 2);
+        return (str);
+}
